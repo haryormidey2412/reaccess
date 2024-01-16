e65a62b02a0a565529a556f5b974005cfdb2d749
Your Name <you@example.com>
2024-01-16 12:01:40 +0100

    commit

---
+++ ass.js
@@ -0,0 +1,13 @@
+// function mar(){
+    // document.querySelector('h1').innerHTML="Fill in your details appropriately";
+// }
+function arm(){
+        document.querySelector('h1').style.color="white";
+        document.querySelector('h1').innerHTML="Fill in your details appropriately";
+        document.querySelector('body').style.background="green";
+}
+
+function mar(){
+        document.querySelector('h3').style.color="green";
+        document.querySelector('h3').innerHTML="Add your CV and a Cover letter";
+}        
